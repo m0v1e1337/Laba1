b789ff14537868a205fa885267e8388f30aad859
Dmitrii Tihonov <dimatihonov873@gmail.com>
2024-03-16 10:07:12 +0300

    Добавьте файлы проекта.

---
+++ Laba1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Laba1.Laba1DataSetTableAdapters;
+
+namespace Laba1
+{
+    public partial class MainWindow : Window
+    {
+        PlayersTableAdapter Players = new PlayersTableAdapter();
+        CoachesTableAdapter Coaches = new CoachesTableAdapter();
+        TeamsTableAdapter Teams = new TeamsTableAdapter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DisplayPlayersData();
+            DisplayCoachesData();
+        }
+
+        private void DisplayPlayersData()
+        {
+            var playersData = Players.GetData();
+
+            DataGrid dataGrid = new DataGrid();
+            dataGrid.AutoGenerateColumns = true;
+            dataGrid.ItemsSource = playersData.DefaultView;
+
+            mainGrid.Children.Add(dataGrid);
+        }
+
+        private void DisplayCoachesData()
+        {
+            var coachesData = Coaches.GetData();
+
+            DataGrid dataGrid = new DataGrid();
+            dataGrid.AutoGenerateColumns = true;
+            dataGrid.ItemsSource = coachesData.DefaultView;
+
+            mainGrid.Children.Add(dataGrid);
+        }
+    }
+}
